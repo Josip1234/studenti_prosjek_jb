3307417754ce09ef0aba7f442540324e32a35f61
Josip Bošnjak <jbosnjak3@gmail.com>
2026-02-09 20:31:52 +0100

    dodan trust proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'blokiraj.listu'=>\App\Http\Middleware\BlokirajListuStudenata::class,
         ]);
+         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
